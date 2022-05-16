1e0e92e0bad8c15dbec448e5390a727931d3f558
Anik Chaudhuri <60659642+AnikChaudhuri@users.noreply.github.com>
2022-05-16 10:21:28 +0530

    Update VB.cu

--- VB.cu
+++ VB.cu
@@ -9,7 +9,10 @@
 #include <cuda.h>
 #include <curand_kernel.h>
 #include <random>
-//This kernel calculates bn and samples from a normal distribution
+#include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
+#include <thrust/reduce.h>
+
 __global__ void nor(double* dlambdan, double lambda0, int dim, int gene, double mu_n, double x_sq, double* dbn, double mu0,
                     double b0,double xb, double* dmean, curandState_t *d_states){
 
@@ -21,7 +24,7 @@ __global__ void nor(double* dlambdan, double lambda0, int dim, int gene, double 
     
 }
 
-//This kernel calculates lambdan and samples from a Gamma distribution with Marsaglia and Tsang sampler (Gamma sampler).
+//Marsaglia and Tsang sampler (Gamma sampler)
 __global__ void gam(double* dlambdan, double lambda0, double dim, double gene, double an, double* dbn, double mu_n,
                     double* dvar, int count2, curandState_t *d_states){
     int idx = threadIdx.x + blockDim.x * blockIdx.x;
@@ -54,22 +57,28 @@ __global__ void setup(curandState_t *d_states, int j)
 
 int main(){
     int gene = 30;
-    int dim = 2;
-    double* ex_a = new double[dim*gene]; 
-    //double* mean = new double[1000]; 
-    //double* var = new double[1000];
+    int dim = 20;
+    
+    std::vector<double> ex_a(dim*gene);
     std::default_random_engine generator;
     std::normal_distribution<double> distribution(10.0,2.0);
 
-    for(int i = 0; i < dim*gene; i++){ex_a[i] = distribution(generator);} //generating data
-    for(int i = 0; i < dim*gene; i++) std::cout << ex_a[i] <<std::endl; //printing data
-    double xb = 0; double x_sq = 0; double a = 0.3; double lambda0 = 1; double mu0 = 2; double b0 = 1.0; 
+    for(int i = 0; i < dim*gene; i++){ex_a[i] = distribution(generator);} 
+    //for(int i = 0; i < dim*gene; i++) std::cout << ex_a[i] <<std::endl;
+    
+    double a = 0.3; double lambda0 = 1; double mu0 = 2; double b0 = 1.0; 
+
+    thrust::device_vector<double> d1(gene*dim);
+    thrust::device_vector<double> d2(gene*dim);
+
+    d1 = ex_a;
+    double xb = thrust::reduce(d1.begin(), d1.end());
+    thrust::transform(d1.begin(), d1.end(), d2.begin(), thrust::square<double>());
+    double x_sq = thrust::reduce(d2.begin(), d2.end());
+    
 
     double an = a + ((dim*gene)+1)/2.0; double* bn = new double[1]; bn[0] = 0.2;
-    for(int i = 0; i< dim*gene; i++){
-        xb = xb + ex_a[i];
-        x_sq = x_sq + (ex_a[i]*ex_a[i]);
-    }
+
     double xbar = xb/(dim*gene);
     double mu_n = (lambda0*mu0 + dim*gene*xbar)/(lambda0 + (dim*gene));
     
@@ -89,20 +98,20 @@ int main(){
     cudaMemcpy(dbn, bn, sizeof(double)*1, cudaMemcpyHostToDevice);
     cudaMemcpy(dlambdan, lambdan, sizeof(double)*1, cudaMemcpyHostToDevice);
 
-    curandState_t* d_states;//used in random number generation
+    curandState_t* d_states;
     cudaMalloc(&d_states, sizeof(curandState_t)*1);
 
-    cudaStream_t stream1, stream2; //CUDA streams
+    cudaStream_t stream1, stream2;
     cudaStreamCreate(&stream1); cudaStreamCreate(&stream2); 
     int count2 = 0; 
+    
     for(int i =0; i < 1000; i++){
         setup<<<1,1>>>(d_states,i);//setting up random numbers
 
-        nor<<<1,1,0,stream1>>>(dlambdan, lambda0, dim, gene, mu_n, x_sq, dbn, mu0,b0,xb, dmean, d_states);//Samples from the normal distribution on stream 1
+        nor<<<1,1,0,stream1>>>(dlambdan, lambda0, dim, gene, mu_n, x_sq, dbn, mu0,b0,xb, dmean, d_states);
 
-        gam<<<1,1,0,stream2>>>(dlambdan, lambda0, dim, gene, an, dbn, mu_n, dvar, count2, d_states);//Samples from the gamma distribution on stream 2
-        
-        //copying back the samples from device to host
+        gam<<<1,1,0,stream2>>>(dlambdan, lambda0, dim, gene, an, dbn, mu_n, dvar, count2, d_states);
+
         cudaMemcpy(bn, dbn, sizeof(double)*1, cudaMemcpyDeviceToHost);
         //cudaMemcpy(lambdan, dlambdan, sizeof(double)*1, cudaMemcpyDeviceToHost);
         cudaMemcpy(meancopy, dmean, sizeof(double)*1, cudaMemcpyDeviceToHost);
